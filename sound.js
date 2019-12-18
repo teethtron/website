58a45c95308a35638e04117a5bb3cf1c8153fbdd
teethtron <40450650+teethtron@users.noreply.github.com>
2019-12-18 01:27:50 -0700

    Add files via upload

---
+++ sound.js
@@ -0,0 +1,24 @@
+var z = document.getElementById("a1");
+var x = document.getElementById("a2");
+var y = document.getElementById("a3");
+var w = document.getElementById("a4");
+var v = document.getElementById("a5");
+function play1() { 
+  z.play(); 
+} 
+
+function play2() { 
+  x.play(); 
+} 
+
+function play3() { 
+  y.play(); 
+}
+
+function play4() { 
+  w.play(); 
+}  
+
+function play5() { 
+  v.play(); 
+} 
